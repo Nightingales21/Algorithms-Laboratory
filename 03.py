b8b412cc2807916b8d2e1e91158b3e72edca5e3d
Nightingales21 <nitin.harish21@gmail.com>
2026-01-11 17:11:22 +0000

    qsort

--- 03.py
+++ 03.py
@@ -1,27 +1,36 @@
-def partition1 (a, low, high):
+def partition1(a, low, high):
     pivot = a[high]
     i = low
-    j = high - 1
-    while (i<j):
-        if (a[i] < pivot):
-            i+=1
-        else:
-            a[i], a[j] = a[j], a[i]
-            j-=1
-      
-    print(a)
-    return j+1
+    j = high
 
-def partition (a, low, high):
+    while i < j:
+        if a[i] < pivot:
+            i += 1
+        else:
+            j -= 1
+            a[i], a[j] = a[j], a[i]
+
+    return i
+
+
+def partition(a, low, high):
     pivot = a[high]
     i = low
-    j = high - 1
-    while (i<j):
-        if (a[i] < pivot):
-            i+=1
+    j = high
+
+    while i < j:
+        if a[i] < pivot:
+            i += 1
         else:
+            j -= 1
             a[i], a[j] = a[j], a[i]
-            j-=1
-    a[i], a[high] = a[high], a[i]        
+    a[i], a[high] = a[high], a[i]
+    return i
+
+def qsort(a, low, high):
+    if low > high:                     
+        return a
+    p = partition(a, low, high)     
+    qsort(a, low, p - 1)
+    qsort(a, p + 1, high)           
     return a
-
